d7b33ac060d9342d9b8df576c45604f87ec6c426
tyulpin <153257656+tyulpin@users.noreply.github.com>
2024-09-30 23:29:37 -0500

    Update mealplan.go

--- mealplan/mealplan.go
+++ mealplan/mealplan.go
@@ -1,1 +1,33 @@
+package mealplan
+
+import "fmt"
+
+type Meal struct {
+	Name string
+	Type string
+}
+
+type MealPlan struct {
+	Days map[string][]Meal
+}
+
+func New() *MealPlan {
+	return &MealPlan{
+		Days: make(map[string][]Meal),
+	}
+}
+
+func (mp *MealPlan) AddMeal(day, mealType, name string) {
+	meal := Meal{Name: name, Type: mealType}
+	mp.Days[day] = append(mp.Days[day], meal)
+}
+
+func (mp *MealPlan) View() {
+	for day, meals := range mp.Days {
+		fmt.Printf("\n%s:\n", day)
+		for _, meal := range meals {
+			fmt.Printf("  %s: %s\n", meal.Type, meal.Name)
+		}
+	}
+}
 
